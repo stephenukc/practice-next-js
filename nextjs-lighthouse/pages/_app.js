f338e3d8c345fe39b4fed5ee3caa473a4261e2f0
Stephen Kalu <stephenukc@gmail.com>
2022-09-05 18:03:21 +0100

    Complete nextjs-lighthouse

---
+++ nextjs-lighthouse/pages/_app.js
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+
+export default MyApp;
